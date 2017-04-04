ae7c29353ed075196d132b78fe3b566f03ffc00a
github username <github email address>
2017-04-04 15:49:01 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
+# App
+#   POST /reverse
+#     responds with a 200
+#     displays the reversed string (FAILED - 1)
+#   GET /friends
+#     responds with a 200
+#     displays friends in the view (FAILED - 2)
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -7,11 +14,14 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    @string = params[:string].reverse!
     erb :reversed
   end
 
   get '/friends' do
     #write your code here
+    @friends = ["Charlie Chaplin", "Richard Pryor", "Eddie Murphy", "Louis CK", "Jerry Seinfeld"]
+    erb :friends
   end
 
 end
